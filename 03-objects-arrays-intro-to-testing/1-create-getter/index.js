235cf4f9b56d8b0f7f962b541b65022ac0089ed0
Frolova Mariia <maralfrolova@beeline.ru>
2025-12-20 12:14:28 +0300

    #03-task Объекты и массивы

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function(obj) {
+    return path.split('.').reduce((current, key) => {
+      return current && typeof current === 'object' && current.hasOwnProperty(key) ? current[key] : undefined;
+    }, obj);
+  };
 }
